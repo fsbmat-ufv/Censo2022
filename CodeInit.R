16d2c4b123458abd22e2c6d1e8fca2e83f2bc72d
fsbmat-ufv <fernando.bastos@ufv.br>
2024-12-08 16:31:49 -0300

    dicionarioDados

--- CodeInit.R
+++ CodeInit.R
@@ -7,25 +7,43 @@ library("data.table")
 library("abjutils")
 #setwd("~/Github/DadosCenso2022")
 url <- "~/GitHub/Censo2022/Dados/Agregados_por_municipios_alfabetizacao_BR.csv"
+#url <- "~/GitHub/Censo2022/Dados/Agregados_por_setores_alfabetizacao_BR.csv"
+
 dt <- fread(url,
             encoding = "Latin-1",
             sep = ";",
             header = TRUE)
+
 #dt <- dt %>% select(c(1, 3:15, 107:119))
 #names(dt)
 #dt$NM_MUN <- str_trim(dt$NM_MUN) #Remove espacos em Branco
 #dt$NM_MUN <- str_to_upper(dt$NM_MUN) #Caixa Alta
 #dt$NM_MUN <- rm_accent(dt$NM_MUN) #Remove acentos
+#Coluna Total Brasil
+
+dt <- dt %>%
+        mutate(Total = rowSums(select(., V00644:V00656)))
+Total <- dt %>% select(Total) %>% summarise(sum(Total))
+#Coluna Total de Alfabetizados do Brasil
+
+dt <- dt %>%
+        mutate(Alfab = rowSums(select(., V00748:V00760)))
+Alfab <- dt %>% select(Alfab) %>% summarise(sum(Alfab))
+
 #Coluna 65 anos ou mais por cidade
 
 dt <- dt %>%
         mutate(V06599 = rowSums(select(., V00654, V00655, V00656)))
+
 #Coluna 65 anos ou mais por cidade de alfabetizados
 dt <- dt %>%
         mutate(V07599 = rowSums(select(., V00758, V00759, V00760)))
 
 #Taxa de Analfabetismo por Cidade por faixa etaria
-
+# dt[dt == 'X'] <- 0
+# str(dt$V00644)
+# dt$V00644 <- as.numeric(as.character(dt$V00644))
+Total1519 <- sum(dt$V00644)
 dt$alf1519 <- round(100*((dt$V00644-dt$V00748)/dt$V00644), digits = 1)
 dt$alf2024 <- round(100*((dt$V00645-dt$V00748)/dt$V00645), digits = 1)
 dt$alf2529 <- round(100*((dt$V00646-dt$V00748)/dt$V00646), digits = 1)
@@ -60,7 +78,27 @@ TotBr5054 <- round(100*(sum(dt$V00651)-sum(dt$V00755))/sum(dt$V00651), digits = 
 TotBr5559 <- round(100*(sum(dt$V00652)-sum(dt$V00756))/sum(dt$V00652), digits = 1)
 TotBr6064 <- round(100*(sum(dt$V00653)-sum(dt$V00757))/sum(dt$V00653), digits = 1)
 TotBr6599 <- round(100*(sum(dt$V06599)-sum(dt$V07599))/sum(dt$V06599), digits = 1)
-TotBr6599 <- round(100*(sum(c(dt$V00654, dt$V00655, dt$V00656))-sum(c(dt$V00758, dt$V00759, dt$V00760)))/sum(c(dt$V00654, dt$V00655, dt$V00656)), digits = 1)
+#TotBr6599 <- round(100*(sum(c(dt$V00654, dt$V00655, dt$V00656))-sum(c(dt$V00758, dt$V00759, dt$V00760)))/sum(c(dt$V00654, dt$V00655, dt$V00656)), digits = 1)
+
+# Criar o data.frame
+resultado <- data.frame(
+        "15 a 19 anos" = TotBr1519,
+        "20 a 24 anos" = TotBr2024,
+        "25 a 29 anos" = TotBr2529,
+        "30 a 34 anos" = TotBr3034,
+        "35 a 39 anos" = TotBr3539,
+        "40 a 44 anos" = TotBr4044,
+        "45 a 49 anos" = TotBr4549,
+        "50 a 54 anos" = TotBr5054,
+        "55 a 59 anos" = TotBr5559,
+        "60 a 64 anos" = TotBr6064,
+        "65 ou mais"  = TotBr6599,
+        row.names = "Censo 2022", # Define o nome da linha como 'Censo 2022'
+        check.names = FALSE
+)
+
+# Exibir o resultado
+htmlTable::htmlTable(resultado, caption = "Taxa de Não Alfabetizados")
 
 #Taxa de analfabetismo das pessoas de 15 anos ou mais de idade, segundo a cor ou raca
 #Coluna Totais Brancos
@@ -243,6 +281,7 @@ TxBrIndigenas <- round(100*(sum(dt$Indigenas)-sum(dt$IndigenasAlf))/sum(dt$Indig
 
 ##################################################################################
 library(geobr)
+library(sf)
 # carregando shape files de todos municipios do Brasil
 mun <- read_municipality(code_muni="all", year=2022)
 ###############Juntando os bancos de dados
@@ -253,14 +292,18 @@ analf <- dt %>%
         st_drop_geometry() %>% 
         select(4,50) %>% 
         group_by(abbrev_state) %>% 
-        summarise(Tx=sum(TXAnalf)/n(), quant=n()) %>% 
+        summarise(Tx=sum(dt$TXAnalf)/n(), quant=n()) %>% 
         arrange(Tx)
 
+TotEst <- dt %>% 
+        st_drop_geometry() %>% 
+        group_by(name_state) %>% 
+        summarise(Alf1519=sum(V00644))
 
 ggplot() + geom_sf(data=dt, aes(fill= alf1519))
 
 library(leaflet)
-library(sf)
+
 library(htmltools)  # Necessário para usar `HTML()`
 # Corrigir o CRS para WGS84
 dt <- dt %>%
