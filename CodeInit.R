1231af5ff79b78c0ad776977cb404a7d761e94a5
Fernando Bastos <fernando.bastos@ufv.br>
2025-03-31 12:10:46 -0300

    Posicional

--- CodeInit.R
+++ CodeInit.R
@@ -9,7 +9,49 @@ library("abjutils")
 #url <- "https://raw.githack.com/fsbmat-ufv/Censo2022/refs/heads/main/Dados/Agregados_por_municipios_alfabetizacao_BR.csv"
 #url <- "~/GitHub/Censo2022/Dados/Agregados_por_setores_alfabetizacao_BR.csv"
 url <- "Dados/Populacao_residente_por_situacao_do_domicilio_municipios.csv"
+library(data.table)
 
+arquivo <- "Dados/RO/Amostra_Domicilios_11.txt"
+
+# Vetores widths e colnames (inserir os acima aqui)
+widths <- c(
+        2, 5, 13, 8, 16, 1, 2, 3, 2, 1, 2, 2, 1, 6, 9, 1, 2, 3, 2, 3,
+        1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        1, 1, 7, 10, 8, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1)
+
+col_names <- c(
+        "V0001", "V0002", "V0011", "V0300", "V0010", "V1001", "V1002", "V1003", "V1004", "V1006",
+        "V4001", "V4002", "V0201", "V2011", "V2012", "V0202", "V0203", "V6203", "V0204", "V6204",
+        "V0205", "V0206", "V0207", "V0208", "V0209", "V0210", "V0211", "V0212", "V0213", "V0214",
+        "V0215", "V0216", "V0217", "V0218", "V0219", "V0220", "V0221", "V0222", "V0301", "V0401",
+        "V0402", "V0701", "V6529", "V6530", "V6531", "V6532", "V6600", "V6210", "M0201", "M2011",
+        "M0202", "M0203", "M0204", "M0205", "M0206", "M0207", "M0208", "M0209", "M0210", "M0211",
+        "M0212", "M0213", "M0214", "M0215", "M0216", "M0217", "M0218", "M0219", "M0220", "M0221",
+        "M0222", "M0301", "M0401", "M0402", "M0701", "V1005")
+length(col_names)
+
+# Leitura do arquivo posicional
+
+dados <- readr::read_fwf(
+        file = arquivo,
+        fwf_widths(widths, col_names = col_names),  # <- aqui sim colocamos os nomes
+        col_types = cols(.default = "c"),           # lê tudo como caractere para evitar problemas
+        progress = TRUE
+)
+
+
+sum(widths)
+linha <- readLines(arquivo, n = 1)
+nchar(linha)
+# Converter para data.table (opcional, mas útil!)
+dados <- as.data.table(dados)
+print(dados)
+
+# Visualize as primeiras linhas
+print(head(dados))
+linhas <- readLines(arquivo, n = 5)
+cat(linhas, sep = "\n")
 
 dt <- fread(url,
             encoding = "Latin-1",
